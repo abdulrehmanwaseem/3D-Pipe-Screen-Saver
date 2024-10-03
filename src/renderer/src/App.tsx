52dd82a5e2a6fbd91f894d3b08b3a99f40da2546
abdulrehmanwaseem <abdulrehman.code1@gmail.com>
2024-10-03 12:48:56 +0500

    Add monitor.js file for perf monitoring

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,20 +2,29 @@ import './assets/main.css'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import Pipe from './components/Pipe'
+import { useEffect } from 'react'
+import { monitorPerformance } from './utils/monitor.js'
 
 const App = () => {
+  useEffect(() => {
+    const cleanup = monitorPerformance()
+    return cleanup
+  }, [])
+
   return (
     <div className="w-screen h-screen relative">
-      <h1 className="absolute text-3xl top-4 z-10 left-[40%] font-bold underline">
-        Pipe Screen Saver
-      </h1>
+      <div className="flex justify-center">
+        <h1 className="absolute text-3xl top-4 z-10 font-bold underline">Pipe Screen Saver</h1>
+      </div>
+
       <Canvas camera={{ position: [0, 0, 50] }}>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         <Pipe />
         <OrbitControls />
       </Canvas>
-      <p className="absolute bottom-2 right-2 p-2 text-2xl text-center mt-2 font-semibold ">
+
+      <p className="absolute bottom-2 right-2 p-2 text-lg text-center mt-2 font-semibold">
         Author: Abdul Rehman
       </p>
     </div>
---
+++ src/renderer/src/utils/monitor.js
@@ -0,0 +1,35 @@
+import Stats from 'stats.js'
+
+export const monitorPerformance = () => {
+  const stats = new Stats()
+  stats.showPanel(0)
+
+  document.body.appendChild(stats.dom)
+
+  stats.dom.style.transform = 'scale(1.5)'
+  stats.dom.style.transformOrigin = 'top left'
+  stats.dom.style.position = 'absolute'
+  stats.dom.style.top = '0px'
+  stats.dom.style.left = '0px'
+  stats.dom.style.zIndex = '1000'
+
+  let panel = 0
+  const cyclePanels = () => {
+    panel = panel === 0 ? 2 : 0
+    stats.showPanel(panel)
+    setTimeout(cyclePanels, 5000)
+  }
+  cyclePanels()
+
+  const animate = () => {
+    stats.begin()
+    stats.end()
+    requestAnimationFrame(animate)
+  }
+
+  requestAnimationFrame(animate)
+
+  return () => {
+    document.body.removeChild(stats.dom)
+  }
+}
